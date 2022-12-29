3c5c1105d4e4194b3391f2227ad2b8808724ac25
psp <976477689@qq.com>
2022-12-29 18:05:57 +0800

    init

---
+++ block.ts
@@ -0,0 +1,30 @@
+type TState = 'active' | 'actived'
+type TColor = 'white' | 'black'
+interface IBlock {
+    x: number,
+    y: number,
+    width: number,
+    height: number
+}
+class block {
+    state: TState
+    color: TColor
+    block: IBlock
+    constructor(state: TState, color: TColor, block: IBlock) {
+        this.state = state
+        this.color = color
+        this.block = block
+        this.drop()
+    }
+    drop() {
+        this.block.y++
+        if (this.block.y + this.block.height === 1080 && this.state === 'active' && this.color === 'black') {
+            //emit event of game over 
+        }
+    }
+    changeState() {
+        if (this.color === 'black') {
+            this.color = 'white'
+        }
+    }
+}
