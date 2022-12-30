82bc90e8bc88d7f1277f3d3a85ff3c58c7c13c68
psp <976477689@qq.com>
2022-12-30 17:47:23 +0800

    update

--- src/creator.ts
+++ src/creator.ts
@@ -6,16 +6,21 @@ export default class Creator {
     static ctx: CanvasRenderingContext2D
     static init(targetDom: HTMLElement) {
         this.targetDom = targetDom
-        const { width, height } = targetDom.getBoundingClientRect()
-        const canvas = document.createElement('canvas')
-        canvas.width = width
-        canvas.height = height
-        targetDom.appendChild(canvas)
-        this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
         this.renderUI()
     }
     static renderUI() {
         const { width, height } = this.targetDom.getBoundingClientRect()
+        // create start page
+        const targetDomStyle = {
+            position: 'relative',
+            background: 'black'
+        }
+        // create game page
+        const canvas = document.createElement('canvas')
+        canvas.width = width
+        canvas.height = height
+        this.targetDom.appendChild(canvas)
+        this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
         this.ctx.rect(0, 0, width, height)
         this.ctx.strokeStyle = 'black'
         this.ctx.stroke()
